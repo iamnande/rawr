f054f8b9eb24ddb3dded3a5e9e3493e3329ac5fa
nick anderson <iamnande@users.noreply.github.com>
2025-11-23 15:43:07 -0700

    feat: add role policy wrapper (#1)

---
+++ src/acm.rs
@@ -0,0 +1,72 @@
+use crate::role::{Effect, Role};
+use crate::trie::Trie;
+
+#[derive(Debug)]
+pub struct ACM {
+    allow: Trie,
+    deny: Trie,
+}
+
+impl Default for ACM {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ACM {
+    pub fn new() -> Self {
+        ACM {
+            allow: Trie::new(),
+            deny: Trie::new(),
+        }
+    }
+
+    fn split_action(action: &str) -> Vec<&str> {
+        action.split(':').collect()
+    }
+
+    fn split_resource_path(resource_path: &str) -> Vec<&str> {
+        resource_path.split('/').collect()
+    }
+
+    pub fn allow(&mut self, action: &str, resource_path: &str) {
+        let mut segments = Self::split_action(action);
+        segments.extend(Self::split_resource_path(resource_path));
+        self.allow.insert(&segments);
+    }
+
+    pub fn deny(&mut self, action: &str, resource_path: &str) {
+        let mut segments = Self::split_action(action);
+        segments.extend(Self::split_resource_path(resource_path));
+        self.deny.insert(&segments);
+    }
+
+    pub fn authorized(&self, action: &str, resource_path: &str) -> bool {
+        // cut my life into pieces
+        let mut segments = Self::split_action(action);
+        segments.extend(Self::split_resource_path(resource_path));
+
+        // check the watch list
+        if self.deny.contains(&segments) {
+            return false;
+        }
+
+        // okay, now check "the list"
+        self.allow.contains(&segments)
+    }
+
+    // TODO(nick): find a new home for this. PIP?
+    // this here be PDP land.
+    pub fn apply_role(&mut self, role: &Role) {
+        for policy in &role.policies {
+            for action in &policy.actions {
+                for resource in &policy.resources {
+                    match policy.effect {
+                        Effect::Allow => self.allow(action, resource),
+                        Effect::Deny => self.deny(action, resource),
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/authz.rs
@@ -0,0 +1,59 @@
+use authz::{ACM, Effect, Policy, Role};
+
+#[test]
+fn test_role_policy_application() {
+    let mut acm = ACM::new();
+    let role = Role::new("MHQ.NetworkAdmin", "Lorem Ipsum.")
+        .with_policy(Policy {
+            effect: Effect::Deny,
+            actions: vec!["networks:*".into()],
+            resources: vec!["VLAN-1".into()],
+        })
+        .with_policy(Policy {
+            effect: Effect::Allow,
+            actions: vec!["networks:GetVLAN".into()],
+            resources: vec!["*".into()],
+        })
+        .with_policy(Policy {
+            effect: Effect::Allow,
+            actions: vec!["networks:UpdateVLAN".into()],
+            resources: vec!["VLAN-20".into()],
+        })
+        .with_policy(Policy {
+            effect: Effect::Allow,
+            actions: vec!["networks:*".into()],
+            resources: vec!["VLAN-70".into()],
+        })
+        .with_policy(Policy {
+            effect: Effect::Allow,
+            actions: vec!["networks:AddVLANTag".into()],
+            resources: vec!["nick/lab/*".into()],
+        })
+        .with_policy(Policy {
+            effect: Effect::Allow,
+            actions: vec!["calendar:Get*".into()],
+            resources: vec!["laura/*".into()],
+        });
+
+    acm.apply_role(&role);
+
+    let cases = vec![
+        ("networks:GetVLAN", "VLAN-50", true),
+        ("networks:UpdateVLAN", "VLAN-20", true),
+        ("networks:UpdateVLAN", "VLAN-30", false),
+        ("networks:GetVLAN", "VLAN-1", false),
+        ("networks:DeleteVLAN", "VLAN-70", true),
+        ("networks:AddVLANTag", "nick/lab/VLAN-70", true),
+        ("calendar:GetCalendar", "laura/shared-family-calendar", true),
+    ];
+
+    for (action, resource, expected) in cases {
+        let granted = acm.authorized(action, resource);
+        let message = format!(
+            "[AUTHZ] action: {}, resource: {}, expected: {}, granted: {}",
+            action, resource, expected, granted
+        );
+        assert_eq!(granted, expected, "{}", message);
+        println!("{}", message);
+    }
+}
